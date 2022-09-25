279adb8e7ef5ed6423a108ce32f592efa2fd8917
Raul Rodrigues <raulrm.dev@gmail.com>
2022-09-25 17:09:30 -0300

    complete js and css tweaks

---
+++ main.js
@@ -0,0 +1,113 @@
+const cellElements = document.querySelectorAll("[data_cell]");
+
+const board = document.querySelector("[data_board]");
+
+const winningMessageTextElement = document.querySelector(
+  "[data_winning_message_text]"
+);
+
+const winningMessage = document.querySelector("[data_winning_message]");
+
+const restartButton = document.querySelector("[data_restart_button]");
+
+let isCircleTurn;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const startGame = () => {
+  isCircleTurn = false;
+
+  for (const cell of cellElements) {
+    cell.classList.remove("circle");
+    cell.classList.remove("x");
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+
+  setBoardHoverClass();
+  winningMessage.classList.remove("show_winning_message");
+};
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn
+      ? "O Venceu!"
+      : "X Venceu!";
+  }
+
+  winningMessage.classList.add("show_winning_message");
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+const checkForDraw = () => {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains("x") || cell.classList.contains("circle");
+  });
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+const setBoardHoverClass = () => {
+  board.classList.remove("circle");
+  board.classList.remove("x");
+
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+  setBoardHoverClass();
+};
+
+const handleClick = (
+    cell_element) => {
+  // Colocar a marca (X ou Círculo)
+  const cell = cell_element.target;
+  const classToAdd = isCircleTurn ? "circle" : "x";
+
+  placeMark(cell, classToAdd);
+
+  // Verificar por vitória
+  const isWin = checkForWin(classToAdd);
+
+  // Verificar por empate
+  const isDraw = checkForDraw();
+
+  if (isWin) {
+    endGame(false);
+  } else if (isDraw) {
+    endGame(true);
+  } else {
+    // Mudar símbolo
+    swapTurns();
+  }
+};
+
+startGame();
+
+restartButton.addEventListener("click", startGame);
